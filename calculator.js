10712e4275df0932a4e9cf56c67ca2f2a6817c06
yamaso-burger <apple_1228_ringo@yahoo.co.jp>
2022-08-20 10:53:12 +0900

    create index.html and set sendFile to calculator.js

--- calculator.js
+++ calculator.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Set up is done!');
-  })
+    res.sendFile(__dirname + "/index.html");
+  });
   
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
-  })
+  });
