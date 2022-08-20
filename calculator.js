96eb7e56c1a426f39e60856f9bc0746f03b11ff8
yamaso-burger <apple_1228_ringo@yahoo.co.jp>
2022-08-20 12:04:24 +0900

    complete bmiCalculator challenge

--- calculator.js
+++ calculator.js
@@ -9,7 +9,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
-  });
+});
+
+app.get('/bmicalculator', (req, res) => {
+    res.sendFile(__dirname + '/bmiCalculator.html');
+});
 
 app.post('/', (req, res) => {
     var num1 = Number(req.body.num1);
@@ -19,6 +23,15 @@ app.post('/', (req, res) => {
 
     res.send("The result of the calculation is " + result);
 });
+
+app.post('/bmicalculator', (req, res) => {
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+
+    var result = weight / Math.pow(height, 2);
+    res.send("Your BMI is " + result);
+})
+
   
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
