1a70202d00e611bfaef0e4f96d94b1d658219796
yamaso-burger <apple_1228_ringo@yahoo.co.jp>
2022-08-20 11:37:22 +0900

    complete making a calculator application

--- calculator.js
+++ calculator.js
@@ -2,10 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// body parser 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
   });
+
+app.post('/', (req, res) => {
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var result = num1 + num2;
+
+    res.send("The result of the calculation is " + result);
+});
   
-  app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
-  });
+});
